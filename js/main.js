e37b4077d22219687070d6fd940c0fc9e5c5f345
Farrel Ardian <farrelardian123@gmail.com>
2024-07-27 11:02:52 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+  "use strict";
+
+  $(document).ready(function () {
+    $(window).on("scroll", function () {
+      if ($(this).scrollTop() > 200) {
+        $("#header .header-inner").addClass("sticky");
+      } else {
+        $("#header .header-inner").removeClass("sticky");
+      }
+    });
+
+    /*====================================
+          Sticky Header JS
+      ======================================*/
+    $(window).on("scroll", function () {
+      if ($(this).scrollTop() > 100) {
+        $(".header").addClass("sticky");
+      } else {
+        $(".header").removeClass("sticky");
+      }
+    });
+  });
+})(jQuery);
