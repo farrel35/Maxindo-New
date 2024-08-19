b84e2d4f388a8046192d56b7ff7b52341759138c
Farrel Ardian <farrelardian123@gmail.com>
2024-08-19 09:20:42 +0700

    fix js

--- js/main.js
+++ js/main.js
@@ -71,7 +71,40 @@ $(document).ready(function () {
 
   new WOW().init();
 });
-
+$(document).ready(function () {
+  $(".events.slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
 const scrollers = document.querySelectorAll(".scroller");
 
 // If a user hasn't opted in for recuded motion, then we add the animation
